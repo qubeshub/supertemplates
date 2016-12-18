e027fa9f7c44afea14bcb6260af678abe37f52cd
mdlama <mdlama@wm.edu>
2016-12-18 15:33:25 -0500

    Starting with updated template.

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Footer
+ *
+ * Template used for Special Groups. Will now be auto-created
+ * when admin switches group from type HUB to type Special.
+ *
+ * @author     HUBzero
+ * @copyright  December 2015
+ */
+?>
+
+<div class="super-group-footer-wrap">
+	<div class="super-group-footer">
+		&copy; <?php echo gmdate("Y"); ?> HUBzero Foundation, LLC. Template designed by the HUBzero and QUBES Development Teams.
+	</div>
+</div>
