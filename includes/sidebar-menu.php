9bc5c61e9955cbbbc4aac9c5940bdfcf95596fb2
Drew LaMar <mdlama@dev.qubeshub.org>
2016-12-27 17:28:05 -0500

    Code for sidebar branch

---
+++ includes/sidebar-menu.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Sidebar Menu
+ *
+ * Template used for Special Groups. Will now be auto-created
+ * when admin switches group from type HUB to type Special.
+ *
+ * @author     M. Drew LaMar
+ * @copyright  December 2016
+ */
+?>
+
+<ul class="sidebar-nav nav-pills nav-stacked" id="menu">
+	<li class="active">
+		<a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-dashboard fa-stack-1x "></i></span> Dashboard</a>
+	</li>
+	<li>
+		<a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-flag fa-stack-1x "></i></span> Shortcut</a>
+	</li>
+	<li>
+		<a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-cloud-download fa-stack-1x "></i></span>Overview</a>
+	</li>
+	<li>
+		<a href="#"> <span class="fa-stack fa-lg pull-left"><i class="fa fa-cart-plus fa-stack-1x "></i></span>Events</a>
+	</li>
+	<li>
+		<a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-youtube-play fa-stack-1x "></i></span>About</a>
+	</li>
+	<li>
+		<a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-wrench fa-stack-1x "></i></span>Services</a>
+	</li>
+	<li>
+		<a href="#"><span class="fa-stack fa-lg pull-left"><i class="fa fa-server fa-stack-1x "></i></span>Contact</a>
+	</li>
+</ul>
