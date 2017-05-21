6ef0c6c16e5413625f3e91feabc9f660d4e6f649
Drew LaMar <mdlama@dev.qubeshub.org>
2017-05-21 15:11:08 -0400

    Sidebar scrolling works when larger than visibile content areagit status!

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,20 +104,27 @@ jQuery(document).ready(function(jq) {
 	var bannerHeight = $(".super-group-header-overlay").height();
 	var barHeight = $(".super-group-bar").height();
 
+	var scrollingDir = "";
+	var windowTop = 0;
+	var startBarFade = 0;
+	pushingDown = pushingUp = false;
+
+	// Possible problem here - changing window size won't update this - fix later.
+	var thresh = $sidebarWrap.offset().top + $sidebarWrap.outerHeight(true) - $(window).height();
+	console.log("Threshold = " + thresh);
+
 	$(window).scroll(function() {
-		var windowTop = $(this).scrollTop();
+		windowTop = $(this).scrollTop();
 
 		if (windowTop > scrollTop) {
-			// Down
-			// $sidebarWrap[0].style["background-color"] = "#FF0000";
+			scrollingDown = true;
 		} else {
-			// Up
-			// $sidebarWrap[0].style["background-color"] = "#00FF00";
+			scrollingDown = false;
 		}
 		scrollTop = windowTop;
 
 		// Fade effect for "poweredby QUBES"
-		var startBarFade = bannerHeight - barHeight;
+		startBarFade = bannerHeight - barHeight;
 		if (windowTop > startBarFade) {
 			poweredBy.style["opacity"] = Math.max(1 - (4/startBarFade)*(windowTop-startBarFade), 0);
 			poweredBy.style["cursor"] = "default";
@@ -145,16 +152,50 @@ jQuery(document).ready(function(jq) {
 		// Negative padding on $menuWrap is a headache!
 		// Fix sidebar directly under menu after announcements
 		if (windowTop < bannerHeight + $scontainer.height()) {
-			$sidebarWrap.removeClass("sidebar-wrapper-scrolled");
-			$sidebarWrap.css("top", $menuWrap.height());				
-		} else {
-			$sidebarWrap.addClass("sidebar-wrapper-scrolled");
-			$sidebarWrap.css("top", barHeight + $menuWrap.height() + "px");
+			$sidebarWrap.removeClass("sidebar-wrapper-fixed-top");
 			$sidebarWrap.css("bottom", "");
+			// $sidebarWrap.css("top", $menuWrap.height());				
+		} else {
+			// $sidebarWrap.addClass("sidebar-wrapper-scrolled");
+			// $sidebarWrap.css("top", barHeight + $menuWrap.height() + "px");
+			// $sidebarWrap.css("bottom", "");
+
+			pushingUp = !scrollingDown && (($sidebarWrap.offset().top + 1) > 
+				(parseFloat($sidebarWrap.css("margin-top")) + $menuWrap.offset().top + $menuWrap.height()));
+			pushingDown = scrollingDown && (($sidebarWrap.offset().top + $sidebarWrap.outerHeight(true) - 1) < 
+				(windowTop + $(this).height()))
+
+			console.log("Pushing Down: " + pushingDown);
+			if (pushingDown) {
+				if (scrollingDown) {
+					$sidebarWrap.css("top","");
+					$sidebarWrap.addClass("sidebar-wrapper-fixed-bottom");
+				}
+			} else {
+				if ((!scrollingDown) && ($sidebarWrap.hasClass("sidebar-wrapper-fixed-bottom"))) {
+					$sidebarWrap.removeClass("sidebar-wrapper-fixed-bottom");
+					$sidebarWrap.css("top", (41 - 13 + windowTop - (thresh)) + "px");
+				}
+			}
+
+			if (pushingUp) {
+				if (!scrollingDown) {
+					$sidebarWrap.css("top","");
+					$sidebarWrap.css("bottom","");
+					$sidebarWrap.addClass("sidebar-wrapper-fixed-top");
+				}
+			} else {
+				if ((scrollingDown) && ($sidebarWrap.hasClass("sidebar-wrapper-fixed-top"))) {
+					$sidebarWrap.removeClass("sidebar-wrapper-fixed-top");
+					// I think this is the proper way - replace bottom one above with a variant of this.
+					$sidebarWrap.css("top", 42 + windowTop - $contentWrap.offset().top + parseFloat($contentWrap.css("padding-top")) + "px");					
+				}
+			}
 
 			// Put sidebar at bottom when footer starts to encroach and scroll with page
-			if (windowTop > (bannerHeight + $menuWrap.height() + $scontainer.height() + $contentWrap.height() - $sidebarWrap.outerHeight(true))) {
-				$sidebarWrap.removeClass("sidebar-wrapper-scrolled");
+			// if (windowTop > (bannerHeight + $menuWrap.height() + $scontainer.height() + $contentWrap.height() - $sidebarWrap.outerHeight(true))) { //
+			if (windowTop + $(this).height() > $footerWrap.offset().top + 41 - 13) {
+				$sidebarWrap.removeClass("sidebar-wrapper-fixed-bottom");
 				$sidebarWrap.css("bottom", "0px");
 				$sidebarWrap.css("top", "");
 			}
